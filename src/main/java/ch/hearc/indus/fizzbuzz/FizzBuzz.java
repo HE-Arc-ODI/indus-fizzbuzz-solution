f9924c08317975f85c85b98db7b5b38b38a5482d
Ulysse Rosselet <urosselet@gmail.com>
2017-11-15 12:13:39 +0100

    list size passes

--- src/main/java/ch/hearc/indus/fizzbuzz/FizzBuzz.java
+++ src/main/java/ch/hearc/indus/fizzbuzz/FizzBuzz.java
@@ -20,7 +20,9 @@ public class FizzBuzz {
    */
   public ArrayList<String> fizzBuzzCheatSheet(int length) {
     ArrayList<String> cheatSheet = new ArrayList(length);
-
+    for(int i = 0; i < length; i++){
+      cheatSheet.add(i, String.valueOf(i+1));
+    }
     return cheatSheet;
   }
 }
