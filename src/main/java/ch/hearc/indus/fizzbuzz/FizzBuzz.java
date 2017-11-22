904e05ce2816bebf91336b3bd599fd4960b5f2cd
Ulysse Rosselet <urosselet@gmail.com>
2017-11-22 10:17:58 +0100

    doc domments, cleanup and improved tests

--- src/main/java/ch/hearc/indus/fizzbuzz/FizzBuzz.java
+++ src/main/java/ch/hearc/indus/fizzbuzz/FizzBuzz.java
@@ -8,42 +8,27 @@ import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 /**
- *
- *
+ * FizzBuzz cheatsheet generator stage 1. The generator prints the numbers from 1 to 100. But for
+ * multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
+ * For numbers which are multiples of both three and five print “FizzBuzz".
  */
 public class FizzBuzz {
 
   /**
-   *
-   *
-   * @param length
-   * @return
+   * Generates the FizzBuzz game cheatsheet of the given length.
+   * @param length length of the cheatsheet
+   * @return the cheatsheet.
    */
   public ArrayList<String> fizzBuzzCheatSheet(int length) {
-    ArrayList<String> cheatSheet = new ArrayList(length);
+    ArrayList<String> cheatSheet = new ArrayList<String>(length);
     for (int i = 0; i < length; i++) {
       cheatSheet.add(i, String.valueOf(i + 1));
     }
     return replaceMultiplesOf3and5ByFizzBuzz(cheatSheet);
   }
 
-  public ArrayList<String> replaceMultiplesOf3ByFizz(ArrayList<String> list) {
-    return list.stream().map(s -> {
-      int n = Integer.parseInt(s);
-      if ((n % 3) == 0 && (n % 5) == 0) {
-        return "FizzBuzz";
-      } else if ((n % 3) == 0) {
-        return "Fizz";
-      } else if ((n % 5) == 0) {
-        return "Buzz";
-      } else {
-        return s;
-      }
-    }).collect(
-        Collectors.toCollection(ArrayList::new));
-  }
 
-  public ArrayList<String> replaceMultiplesOf3and5ByFizzBuzz(ArrayList<String> list) {
+  private ArrayList<String> replaceMultiplesOf3and5ByFizzBuzz(ArrayList<String> list) {
     return list.stream().map(s -> {
       int n = Integer.parseInt(s);
       String str = "";
--- src/test/java/ch/hearc/indus/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/ch/hearc/indus/fizzbuzz/FizzBuzzTest.java
@@ -5,6 +5,7 @@
 package ch.hearc.indus.fizzbuzz;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import org.junit.Before;
@@ -12,37 +13,59 @@ import org.junit.Test;
 
 public class FizzBuzzTest {
 
-  public static final int CAPACITY = 100;
+  private static final int CAPACITY = 100;
   private FizzBuzz fb;
-  private ArrayList cheatSheet;
+  private ArrayList<String> cheatSheet;
+
 
   @Before
   public void initFizzBuzz() {
     fb = new FizzBuzz();
     cheatSheet = fb.fizzBuzzCheatSheet(CAPACITY);
+    System.out.println(cheatSheet);
   }
 
   @Test
   public void testListSize() {
     assertEquals(CAPACITY, cheatSheet.size());
-    System.out.println(cheatSheet);
   }
 
   @Test
-  public void testSequence() {
-    for(int i=0; i<CAPACITY;i++){
-      assertEquals(String.valueOf(i+1),cheatSheet.get(i));
+  public void replaceMultiplesOf3ByFizz() {
+    for (int i = 0; i < cheatSheet.size(); i++) {
+      int pos = (i + 1) % 3;
+      if (pos == 0) {
+        if ((i + 1) % 5 == 0) {
+          assertEquals(0, cheatSheet.get(i).indexOf("Fizz"));
+        } else {
+          assertTrue(cheatSheet.get(i).equals("Fizz"));
+        }
+      }
     }
   }
 
   @Test
-  public void replaceMultiplesOf3ByFizz(){
-    System.out.println(fb.replaceMultiplesOf3ByFizz(cheatSheet));
+  public void replaceMultiplesOf5ByBuzz() {
+    for (int i = 0; i < cheatSheet.size(); i++) {
+      int pos = (i + 1) % 5;
+      if (pos == 0) {
+        if ((i + 1) % 3 == 0) {
+          assertEquals(4, cheatSheet.get(i).indexOf("Buzz"));
+        } else {
+          assertTrue(cheatSheet.get(i).equals("Buzz"));
+        }
+      }
+    }
   }
 
   @Test
-  public void replaceMultiplesOf3and5ByFizzBuzz(){
-    System.out.println(fb.replaceMultiplesOf3and5ByFizzBuzz(cheatSheet));
+  public void replaceMultiplesOf3and5ByFizzBuzz() {
+    for (int i = 0; i < cheatSheet.size(); i++) {
+      int pos = (i + 1) % 15;
+      if (pos == 0) {
+        assertTrue(cheatSheet.get(i).contentEquals("FizzBuzz"));
+      }
+    }
   }
 
 }
